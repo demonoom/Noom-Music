b526af730f2893abeed1adb2c2adb7b44a08754f
noom <noom0130@foxmail.com>
2018-05-08 18:15:45 +0800

    router4两种路由嵌套

--- src/components/search/Album.js
+++ src/components/search/Album.js
@@ -1,14 +1,11 @@
 import React from "react"
-import {Route} from "react-router-dom"
-import Album from './Album'
 import "./search.styl"
 
 class Search extends React.Component {
     render() {
         return (
             <div className="music-search">
-                Search
-                <Route path="/search/search2" component={Album}/>
+                烦烦烦
             </div>
         )
     }
